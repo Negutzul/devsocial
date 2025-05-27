d4cb3fc6ccd4dd927ada726b5ae9db8e2e172c29
Negutzul <neaguandrei47@yahoo.com>
2025-05-27 20:44:47 +0300

    added login form

---
+++ DevSocial.Web/src/app/components/auth/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginData = {
+    email: '',
+    password: ''
+  };
+
+  onSubmit() {
+    // TODO: Implement login logic
+    console.log('Login attempt:', this.loginData);
+  }
+} 
