31c53fbee7b2172536492eaf4cb2ec38ffa6fb2f
Negutzul <neaguandrei47@yahoo.com>
2025-05-31 18:04:38 +0300

    logout functionality

--- DevSocial.Web/src/app/components/navbar/navbar.component.ts
+++ DevSocial.Web/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,7 +13,21 @@ import { RouterModule } from '@angular/router';
 export class NavbarComponent {
   isMenuOpen = false;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 }
